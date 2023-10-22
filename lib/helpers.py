812d43a98a8fea3206fa3734b45bb6f55e794e8e
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-10-22 17:42:11 -0500

    add list_genres and find_genre_by_id to helpers

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,7 +1,18 @@
 # lib/helpers.py
+from models.genre import Genre
+from models.movie import Movie
 
-def helper_1():
-    print("Performing useful function#1.")
+def list_genres():
+    genres = Genre.get_all()
+    for genre in genres:
+        print(genre)
+
+def find_genre_by_id():
+    _id = input("Enter the movie's id: ")
+    genre = Genre.find_by_id(_id)
+    print(genre) if genre else print(f"Genre {_id} not found.")
+
+    
 
 
 def exit_program():

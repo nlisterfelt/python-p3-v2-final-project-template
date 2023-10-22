fb3b60dace8f4e0d8596fad35314edf6078e20c2
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-10-22 15:04:08 -0500

    add genre setter methods

--- lib/models/genre.py
+++ lib/models/genre.py
@@ -1,3 +1,43 @@
 from models.__init__ import CURSOR, CONN
 
 class Genre:
+    all = {}
+
+    def __init__(self, name, id=None):
+        self.name = name
+        self.id = id
+
+    def __repr__(self):
+        return f"<Genre {self.id}: {self.name}>"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string.")
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS genres (
+            id INTEGER PRIMARY KEY,
+            name TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS genres;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        

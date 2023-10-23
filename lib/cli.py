4c3150e7ecad227157d819641468196c4ea31318
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-10-23 13:06:43 -0500

    add 3 options to the main menu to search for genres

--- lib/cli.py
+++ lib/cli.py
@@ -21,82 +21,63 @@ from helpers import (
 def main():
     choice = "0"
     while choice != "e":
-        if choice == "0":
-            print("*****Main Menu*****")
-            print("Please select an option:")
-            print("1. List all genres.")
-            print("e. Exit the program.")
-            print("*******************")
-            choice = input("> ")
+        main_menu()
+        choice = input("> ")
         
-        #Entering genres module
-        if choice == "1":
+        #Entering Genre menu
+        if choice == "g":
             genre_choice = "0"
-            print("-----Genre Menu-----")
-            print("Select an option:")
-            list_genres()
-            print("c. Create new genre.")
-            print("n. Find genre by name.")
-            print("m. Main Menu")
-            print("e. Exit the program.")
+            while genre_choice != "m":
+                genre_menu()
+                genre_choice = input("> ")
+
+                if genre_choice == "c":
+                    create_genre()
+                #Entering Movie menu
+                elif genre_choice == "n":
+                    find_genre_by_name()
+                elif genre_choice == "i":
+                    find_genre_by_id()
+
+                elif genre_choice == "e":
+                    exit_program()
+                else:
+                    find_genre_by_id()
         elif choice == "e":
             exit_program()
         else:
             print("Invalid choice")
 
 
-def main_menu():
-
-
-
 if __name__ == "__main__":
     main()
 
+def main_menu():
+    print('''
 
+*****Main Menu*****
 
-"""        
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. List all genres.")
-    print("2. Find a genre by id.")
-    print("3. Find a genre by name.")
-    print("4. Create a genre.")
-    print("5. Update a genre.")
-    print("6. Delete a genre.")
-    print("7. List all movies.")
-    print("8. Find a movie by id.")
-    print("9. Find a movie by title.")
-    print("10. Find all movies greater than or equal to a run-time.")
-    print("11. Create a movie.")
-    print("12. Update a movie.")
-    print("13. Delete a movie.")
-    
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            list_genres()
-        elif choice == "2":
-            find_genre_by_id()
-        elif choice == "3":
-            find_genre_by_name()
-        elif choice == "4":
-            create_genre()
-        elif choice == "5":
-            update_genre()
-        elif choice == "6":
-            delete_genre()
-        elif choice == "7":
-            list_movies()
-        elif choice == "8":
-            find_movie_by_id()
-        elif choice == "9":
-            find_movie_by_title()
-        elif choice == "10":
-            find_movies_by_run_time()
-        elif choice == "11":
-            create_movie()
-        elif choice == "12":
-            update_movie()
-        elif choice == "13":
-            delete_movie()
-"""
+    Select an option:
+    1. List all genres.
+    2. Find genre by id.
+    3. Find genre by name.
+    e. Exit the program.
+
+*******************
+
+''')
+
+def genre_menu():
+    print(f'''
+-----Genre Menu-----
+ID: Name''')
+    list_genres()       
+    print('''
+                        
+    Select an option:
+    c. Create new genre.
+    n. Find genre by name.
+    m. Main Menu
+    e. Exit the program.
+                
+--------------------''')

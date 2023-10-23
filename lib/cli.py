463d9c2da9b58ff8fa63abb0b593f16b79481407
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-10-22 19:20:43 -0500

    add imports to cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,8 +1,20 @@
 # lib/cli.py
 
 from helpers import (
-    exit_program,
-    helper_1
+    list_genres,
+    find_genre_by_id,
+    find_genre_by_name,
+    create_genre,
+    update_genre,
+    delete_genre,
+    list_movies,
+    find_movie_by_id,
+    find_movie_by_title,
+    find_movies_by_run_time,
+    create_movie,
+    update_movie,
+    delete_movie,
+    exit_program
 )
 
 
@@ -21,7 +33,18 @@ def main():
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    print("4. ")
+    print("5. ")
+    print("6. ")
+    print("7. ")
+    print("8. ")
+    print("9. ")
+    print("10. ")
+    print("11. ")
+    print("12. ")
 
 
 if __name__ == "__main__":

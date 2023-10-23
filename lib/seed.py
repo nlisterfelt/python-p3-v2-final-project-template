6ee8aa5a3bb93f0edb5c8cc9672f991044a000e6
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-10-22 19:58:03 -0500

    add genre and movie to seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -3,3 +3,13 @@ from models.genre import Genre
 from models.movie import Movie
 
 def seed_database():
+    Movie.drop_table()
+    Genre.drop_table()
+    Movie.create_table()
+    Genre.create_table()
+
+    rom_com = Genre.create("Romantic comedy")
+    Movie.create("The Proposal", 108, rom_com.id)
+
+seed_database()
+print("Seeded database.")
